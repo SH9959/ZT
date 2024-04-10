5aba8aafda9d1ec6832b6cc54410fc2f41969e5b
SH9959 <1572958043@qq.com>
2024-04-10 20:49:01 +0800

    hsong

--- voice_pkg/scripts/interrupt_old/CLS/guide_words.py
+++
@@ -1,5 +0,0 @@
-def guide_words(task_type):
-    if task_type == '打断':
-        gid_wrd = '请问您有什么问题吗'
-    elif task_type == '问答':
-        gid_wrd = '请问您还有其他问题吗'
--- voice_pkg/scripts/interrupt_old/LLM/__init__.py
+++
@@ -1,5 +0,0 @@
-from .apply import text_correct
-from .apply import sleep_judge
-
-from .testLLM import DocumentQAGPT4
-from .testLLM import DocumentQAHuoZi
